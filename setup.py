8860388e987d19624cbf63c6a780ba1010eaefa5
ermalism <e.ismajli@celonis.de>
2020-09-21 01:58:41 +0200

    add readme as long description for pip package

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme_content = f.read()
 setup(
     name='slugify-cli',
     version='0.3',
     description='Slugify CLI Tool',
+    long_description=readme_content,
+    long_description_content_type='text/markdown',
     url='https://github.com/ermalism/slugify-cli',
     author='ermalism',
     author_email='ismajliermal@gmail.com',
