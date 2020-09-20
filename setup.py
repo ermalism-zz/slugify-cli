a2635083e3ff9bda7a780a77c97b08080f6753a0
ermalism <e.ismajli@celonis.de>
2020-09-20 23:03:45 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='slugify-cli',
-    version='0.1',
+    version='0.2',
     description='Slugify CLI Tool',
     url='https://github.com/ermalism/slugify',
     author='ermalism',
@@ -11,5 +11,5 @@ setup(
     scripts=['bin/slugify-cli'],
     python_requires='>3.5.2',
     zip_safe=False,
-    download_url='https://github.com/ermalism/slugify-cli/archive/v0.1.tar.gz'
+    download_url='https://github.com/ermalism/slugify-cli/archive/v0.2.tar.gz'
 )
